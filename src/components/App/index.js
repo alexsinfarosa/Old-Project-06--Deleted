462ca7f6cd583fd9117108cae853cb7a76d7121b
alexsinfarosa <as898@cornell.edu>
2017-08-04 10:11:17 +0200

    first commit

---
+++ src/components/App/index.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import { inject, observer } from "mobx-react";
+import { when } from "mobx";
+
+// components
+import Home from "views/Home";
+
+// api
+import { fetchAllStations } from "utils/api";
+
+@inject("store")
+@observer
+class App extends Component {
+  constructor(props) {
+    super(props);
+    const protocol = this.props.store.app.protocol;
+    when(
+      // once...
+      () => this.props.store.app.stations.length === 0,
+      // ... then
+      () =>
+        fetchAllStations(protocol).then(allStations =>
+          this.props.store.app.setStations(allStations)
+        )
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <Home />
+      </div>
+    );
+  }
+}
+
+export default App;
