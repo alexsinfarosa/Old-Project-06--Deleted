9fcd0c184a02c641f43a349c3a0776636615dd75
Alex Sinfarosa <as898@cornell.edu>
2018-05-21 11:33:06 -0400

    updates

--- src/components/Station/index.js
+++ src/components/Station/index.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
-import { inject, observer } from 'mobx-react';
+import React, { Component } from "react";
+import { inject, observer } from "mobx-react";
 // import { toJS } from "mobx";
 // import { Select } from "antd";
-import Select from 'antd/lib/select';
-import 'antd/lib/select/style/css';
+import Select from "antd/lib/select";
+import "antd/lib/select/style/css";
 const Option = Select.Option;
 
 // Utilities
 // import { states } from "../states";
 
-@inject('store')
+@inject("store")
 @observer
 class State extends Component {
   handleChange = async value => {
@@ -25,14 +25,14 @@ class State extends Component {
   render() {
     const { getCurrentStateStations, getStation } = this.props.store.app;
 
-    const stationList = getCurrentStateStations.map(station =>
+    const stationList = getCurrentStateStations.map(station => (
       <Option key={`${station.id} ${station.network}`} value={station.name}>
         {station.name}
       </Option>
-    );
+    ));
 
     return (
-      <div style={{ marginBottom: '2rem' }}>
+      <div style={{ marginBottom: "2rem" }}>
         <label>Station:</label>
         <Select
           name="station"
